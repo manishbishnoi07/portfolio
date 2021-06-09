416ff9ae6ecba4d680181da47bb58563d296ae6b
Manish Bishnoi <manishbishnoi897@gmail.com>
2021-06-10 03:43:44 +0530

    Added skills assets

---
+++ src/assets/skillsData.js
@@ -0,0 +1,58 @@
+import Skill1 from "./skill1.png";
+import Skill2 from "./skill2.png";
+import Skill3 from "./skill3.png";
+import Skill4 from "./skill4.jpg";
+import Skill5 from "./skill5.png";
+import Skill6 from "./skill6.jpg";
+import Skill7 from "./skill7.jpg";
+import Skill8 from "./skill8.png";
+import Skill9 from "./skill9.png";
+
+export const skillList = [
+  {
+    skillName: "HTML",
+    skillUrl:
+      "https://user-images.githubusercontent.com/65498562/121435603-d032bf80-c99c-11eb-93c3-5ce9d775c063.png",
+  },
+  {
+    skillName: "CSS",
+    skillUrl:
+      "https://user-images.githubusercontent.com/65498562/121435644-e476bc80-c99c-11eb-8368-b7b063ef44f5.png",
+  },
+  {
+    skillName: "JavaScript",
+    skillUrl:
+      "https://user-images.githubusercontent.com/65498562/121435686-f3f60580-c99c-11eb-8c70-c577e3c6f012.png",
+  },
+  {
+    skillName: "React",
+    skillUrl:
+      "https://user-images.githubusercontent.com/65498562/121435815-243da400-c99d-11eb-8dbc-b487a7506f50.jpg",
+  },
+  {
+    skillName: "Redux",
+    skillUrl:
+      "https://user-images.githubusercontent.com/65498562/121435842-2f90cf80-c99d-11eb-8671-619c0d89424f.png",
+  },
+  {
+    skillName: "NodeJS",
+    skillUrl:
+      "https://user-images.githubusercontent.com/65498562/121435864-3a4b6480-c99d-11eb-8a00-3608943baeff.jpg",
+  },
+  ,
+  {
+    skillName: "MongoDB",
+    skillUrl:
+      "https://user-images.githubusercontent.com/65498562/121435886-446d6300-c99d-11eb-9714-db9304ebd7dd.jpg",
+  },
+  {
+    skillName: "C++",
+    skillUrl:
+      "https://user-images.githubusercontent.com/65498562/121435918-50592500-c99d-11eb-8dc2-acbb69cd120f.png",
+  },
+  {
+    skillName: "MySQL",
+    skillUrl:
+      "https://user-images.githubusercontent.com/65498562/121435948-5b13ba00-c99d-11eb-84f7-a6f71cd3de24.png",
+  },
+];
