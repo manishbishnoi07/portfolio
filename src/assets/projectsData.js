702699f1724443a9c0b22287b91c2c48b9376108
Manish Bishnoi <manishbishnoi897@gmail.com>
2021-06-25 04:06:47 +0530

    Added projectsData JSON file

---
+++ src/assets/projectsData.js
@@ -0,0 +1,38 @@
+export const projectsData = [
+  {
+    projectName: "Whatsapp 2.0",
+    projectDescription:
+      "It is a chat application where users can create rooms for their conversation. It is built on MERN Stack.\n\nUses Pusher for real-time communication between the end users and JWT for authorization.",
+    imageUrl:
+      "https://user-images.githubusercontent.com/65498562/123322702-3e0ce880-d552-11eb-808b-81152ea4327c.png",
+    projectUrl: "https://whatsapp-web-app.netlify.app/",
+    sourceCode: "https://github.com/manishbishnoi07/whatsapp-web",
+  },
+  {
+    projectName: "Notes Drive",
+    projectDescription:
+      "A web application with Google authentication that allows users to share their documents.\n\nUses Redux for managing the state of an application. All files gets stored into Firebase storage.\nMade using ReactJS and Firebase.",
+    imageUrl:
+      "https://user-images.githubusercontent.com/65498562/123322885-6d235a00-d552-11eb-8a0a-1e807d3b5f1a.png",
+    projectUrl: "https://notes-drive.netlify.app/",
+    sourceCode: "https://github.com/manishbishnoi07/notes-drive",
+  },
+  {
+    projectName: "News 24",
+    projectDescription:
+      "Developed a News app with great UI using ReactJS. Users can view all the latest news from all around the world.\n\nAdded a feature for sharing the news over the social media.",
+    imageUrl:
+      "https://user-images.githubusercontent.com/65498562/123335839-a794f300-d562-11eb-893b-86457684ae2f.png",
+    projectUrl: "https://news-24.netlify.app/",
+    sourceCode: "https://github.com/manishbishnoi07/News24-react-project",
+  },
+  {
+    projectName: "Covid-19 Tracker",
+    projectDescription:
+      "Built a Covid-19 Tracker app using React Hooks and Material-UI.\n\nVisualized Covid data in the form of Line Graph using Chart.js.\nUsed React Leaflet for showing the Covid-19 data on the map.",
+    imageUrl:
+      "https://user-images.githubusercontent.com/65498562/123323114-b7a4d680-d552-11eb-81bc-0bcdd7b3108e.png",
+    projectUrl: "https://covid-19-tracker-86b7d.web.app/",
+    sourceCode: "https://github.com/manishbishnoi07/whatsapp-web",
+  },
+];
