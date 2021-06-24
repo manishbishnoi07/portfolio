3c2df25cf4765b9472a3089341b50acbf9dca9dd
Manish Bishnoi <manishbishnoi897@gmail.com>
2021-06-25 04:07:24 +0530

    Added project card

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./ProjectCard.css";
+const ProjectCard = ({
+  imageUrl,
+  projectName,
+  projectDescription,
+  projectUrl,
+  sourceCode,
+  index,
+}) => {
+  return (
+    <div className={`projectCard ${index % 2 === 1 && "projectCard--modify"}`}>
+      <div className="projectCard__img">
+        <img src={imageUrl} alt="project pic" />
+      </div>
+      <div className="projectCard__info">
+        <h2>{projectName}</h2>
+        <p>{projectDescription}</p>
+        <a href={sourceCode} target="_blank" rel="noopener noreferrer">
+          Source Code
+        </a>
+        <a href={projectUrl} target="_blank" rel="noopener noreferrer">
+          Live
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
