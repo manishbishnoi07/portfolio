d654c545864b3702943318d8c023cd399f051499
Manish Bishnoi <manishbishnoi897@gmail.com>
2021-06-24 21:50:17 +0530

    Updated About section

--- src/components/About.js
+++ src/components/About.js
@@ -30,19 +30,40 @@ const About = () => {
           <h3>Hello!</h3>
           <br />
           <p>
-            My name is Manish Bishnoi. I am from Haryana but currently living in
-            Delhi. I am pursuing my Bachelor's degree in Computer Science and
-            Engineering from Amity University, Noida. I am in pre-final year
-            with an aggregate CGPA 9.03. I am also a scholarship holder from
-            last 3 years for my academic performance.
+            My name is Manish Bishnoi. I am a pre-final year student pursuing my
+            Bachelor's degree in Computer Science and Engineering from Amity
+            University, Noida with an aggregate CGPA of 9.03. I am also a
+            scholarship holder from last 3 years for my academic performance.
           </p>
           <br />
           <p>
-            I am a passionate Web Developer and loves to build amazing products.
-            I am also very good at Data Structures and Algorithms. I considered
-            myself as a very focused person and I always work towards my goals
-            in a very efficient manner.
+            I am passionate about Web Development with a keen interest in new
+            technologies and enjoy working outside of my comfort zone.
+            <br />
+            <br />I have a passion for problem solving and software development.
+            I am also very good at Data Structures and Algorithms. Find me on
+            following coding platforms.
           </p>
+          <a
+            href="https://leetcode.com/manishbishnoi897/"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{ display: "inline-block", marginTop: "10px" }}
+          >
+            <Avatar src="https://upload.wikimedia.org/wikipedia/commons/1/19/LeetCode_logo_black.png" />
+          </a>
+          <a
+            href="https://auth.geeksforgeeks.org/user/manishbishnoi897/profile"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              display: "inline-block",
+              marginTop: "10px",
+              marginLeft: "10px",
+            }}
+          >
+            <Avatar src="https://img.icons8.com/color/452/GeeksforGeeks.png" />
+          </a>
         </div>
         <div className="about__bodyImage">
           <Avatar
