b7f59e10bae6318882c8ddcbfe7ab6c427dbbbd0
Manish Bishnoi <manishbishnoi897@gmail.com>
2021-07-10 02:56:03 +0530

    Added scroll effect.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,29 +2,44 @@ import { Avatar } from "@material-ui/core";
 import React from "react";
 import "./Navbar.css";
 import Logo from "../assets/logo.PNG";
+import { Link, animateScroll as Scroll } from "react-scroll";
+
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div className="navbar__left">
-        <h3>
-          <a href="#">
-            <Avatar src={Logo} alt="logo" />
-          </a>
-        </h3>
+      <div
+        className="navbar__left"
+        onClick={() =>
+          Scroll.scrollToTop({
+            offset: -30,
+            smooth: true,
+            duration: 1000,
+          })
+        }
+      >
+        <Avatar src={Logo} alt="logo" />
       </div>
       <div className="navbar__right">
         <ul>
           <li>
-            <a href="#">About</a>
+            <Link to="about" offset={-30} smooth={true} duration={1000}>
+              About
+            </Link>
           </li>
           <li>
-            <a href="#">Skills</a>
+            <Link to="skills" offset={-30} smooth={true} duration={1000}>
+              Skills
+            </Link>
           </li>
           <li>
-            <a href="#">Projects</a>
+            <Link to="projects" offset={-30} smooth={true} duration={1000}>
+              Projects
+            </Link>
           </li>
           <li>
-            <a href="#">Contact</a>
+            <Link to="contact" offset={-30} smooth={true} duration={1000}>
+              Contact
+            </Link>
           </li>
         </ul>
       </div>
