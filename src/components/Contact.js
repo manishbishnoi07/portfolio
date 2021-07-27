32dcc1e09037a11683bae4dcc538bbef194611cd
Manish Bishnoi <manishbishnoi897@gmail.com>
2021-07-28 04:16:50 +0530

    Added contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,27 @@
 import React from "react";
 import "./Contact.css";
 import Header from "./Header";
+import emailjs from "emailjs-com";
+
 const Contact = () => {
-  const handleSubmit = (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        e.target,
+        process.env.REACT_APP_USER_ID
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
   };
 
   return (
@@ -13,7 +31,7 @@ const Contact = () => {
         subHeading="Interested to collaborate? Feel free to drop me an email."
       />
       <div className="contact__formContainer">
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={sendEmail}>
           {/* Email Id Input */}
           <input
             type="email"
